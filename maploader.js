40d56438d99b77e30706e9c056e5a70c5bd1ddc1
Thyraz <tobias.wiedenmann@me.com>
2019-06-05 13:29:29 +0200

    Load and save is now implemented

--- maploader.js
+++ maploader.js
@@ -2,44 +2,43 @@ const fs = require('fs');
 const mqtt = require('mqtt');
 const client = mqtt.connect('mqtt://172.17.2.86');
 
+// Subscribe Topics
 client.on('connect', () => {
-  console.log("Connected");
-  client.subscribe('rockrobo/map/load');
-  client.subscribe('rockrobo/map/save');
+	console.log("Connected");
+	client.subscribe('rockrobo/map/load');
+	client.subscribe('rockrobo/map/save');
 });
 
+// Incoming MQTT message (payload is the desired 'map bundle' name -> /mnt/data/maploader/maps/<name>)
 client.on('message', (topic, message) => {
-  if(topic === 'rockrobo/map/load') {
-    console.log("Received Load Request: " + message);
-  }
+	if(topic === 'rockrobo/map/load') {
+		var source = '/mnt/data/maploader/maps/' + message + '/';
+		var destination = '/mnt/data/rockrobo/';
+		
+		console.log("Received Load Request: " + message);
+		copyFiles(source, destination);
+	}
 
-  if(topic === 'rockrobo/map/save') {
-    var dir = '/mnt/data/maploader/maps/' + message + '/';
-    console.log("Received Save Request: " + message);
+	if(topic === 'rockrobo/map/save') {
+		var source = '/mnt/data/rockrobo/';
+		var destination = '/mnt/data/maploader/maps/' + message + '/';
 
-    if (!fs.existsSync(dir)){
-        console.log('Created directory: ' + dir);
-        fs.mkdirSync(dir);
-    }
-
-    fs.copyFile('/mnt/data/rockrobo/user_map0',  dir + '/user_map0', (err) => {
-      if (err) throw err;
-      console.log('user_map0 was copied to maps/' + message);
-    });
-
-    fs.copyFile('/mnt/data/rockrobo/last_map',  dir + '/last_map', (err) => {
-      if (err) throw err;
-      console.log('last_map was copied to maps/' + message);
-    });
-
-    fs.copyFile('/mnt/data/rockrobo/PersistData_1.data',  dir + '/PersistData_1.data', (err) => {
-      if (err) throw err;
-      console.log('PersistData_1.data was copied to maps/' + message);
-    });
-
-    fs.copyFile('/mnt/data/rockrobo/PersistData_2.data',  dir + '/PersistData_2.data', (err) => {
-      if (err) throw err;
-      console.log('PersistData_2.data was copied to maps/' + message);
-    });
-  }
+		console.log("Received Save Request: " + message);
+		copyFiles(source, destination);
+	}
 });
+
+// Backup or restore map files
+function copyFiles(source, destination) {
+	const files = ['user_map0', 'last_map', 'PersistData_1.data', 'PersistData_2.data'];
+	
+	if (!fs.existsSync(dir)){
+		console.log('Created directory: ' + dir);
+		fs.mkdirSync(dir);
+	}
+	
+	for (i = 0; i < files.length; i++) {
+		fs.copyFileSync(source + files[i], destination + files[i]);
+		console.log('Copied ' + files[i] + ' from ' + source + ' to ' + destination);
+	}
+}
